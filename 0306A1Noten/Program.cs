48862170627e7a633960e714342a060f2c34e7c1
frank paulsen <frobnic+ita@gmail.com>
2019-08-05 12:01:26 +0200

    Projektdateien hinzufügen.

---
+++ 0306A1Noten/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0306A1Noten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
