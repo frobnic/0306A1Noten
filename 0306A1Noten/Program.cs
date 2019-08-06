c07daac20e55636a9b705ad724b2cbbbebd97ff5
frank paulsen <frobnic+ita@gmail.com>
2019-08-06 10:29:48 +0200

    code inserted

--- 0306A1Noten/Program.cs
+++ 0306A1Noten/Program.cs
@@ -10,6 +10,47 @@ namespace _0306A1Noten
     {
         static void Main(string[] args)
         {
+            char n;
+            string t;
+
+            Console.Write("Bitte Note eingeben: ");
+            n = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            switch (n)
+            {
+                case '1':
+                    t = "Sehr Gut";
+                    break;
+
+                case '2':
+                    t = "Gut";
+                    break;
+
+                case '3':
+                    t = "Befriedigend";
+                    break;
+
+                case '4':
+                    t = "Ausreichend";
+                    break;
+
+                case '5':
+                    t = "Mangelhaft";
+                    break;
+
+                case '6':
+                    t = "Totaler Unfug";
+                    break;
+
+                default:
+                    t = "";
+                    break;
+            }
+            if (t == "")
+                Console.WriteLine("Eingabefehler!");
+            else
+                Console.WriteLine("{0} steht für \"{1}\"",n,t);
         }
     }
 }
